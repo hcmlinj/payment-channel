4016d78f20bbe47e4a5b75b07c351856d888fc3b
Jamie <hcmlinj@gmail.com>
2017-07-20 21:35:53 +0800

    update sender to account[1] and recipient to account[2]

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var PaymentChannel = artifacts.require("./PaymentChannel.sol");
 
 module.exports = function (deployer) {
-  deployer.deploy(PaymentChannel, web3.eth.accounts[1], 259200, { value: web3.toWei(10) });
+  deployer.deploy(PaymentChannel, web3.eth.accounts[2], 259200, { from: web3.eth.accounts[1], value: web3.toWei(10) });
 };
